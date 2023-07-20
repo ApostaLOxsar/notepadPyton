32528c79b84c489718fb83cf5d9a313d04c72f81
Stepan_nout <Stepanmaslov@gmail.com>
2023-07-20 15:52:02 +0300

    Добавьте файлы проекта.

---
+++ notepadPyton/interf.py
@@ -0,0 +1,21 @@
+﻿class interfRU:
+    def __init__(self):
+        print("Выбран русский язык")
+
+    def saveNoteToFile():
+        print("Вы выбрали сохранить заметку")
+
+    def readNoteToFile():
+        print("Вы выбрали прочитать из файла")
+
+    def addNote():
+        print("Вы выбрали добавить заметку")
+
+    def editNote():
+        print("Вы выбрали редактировать заметку")
+
+    def deletNote():
+        print("Вы выбрали удалить заметку")
+
+    def exitNotepade():
+        print("Вы уходите(( Мы ждем вас снова")
---
+++ notepadPyton/notepadPyton.py
@@ -0,0 +1,30 @@
+﻿import interf 
+from datetime import datetime
+import json
+
+
+interface = interf.interfRU()
+
+curentDay = datetime.today()
+print("%d-%d-%d %d:%d:%d" %(curentDay.day, curentDay.month, curentDay.year, curentDay.hour, curentDay.minute, curentDay.second));
+
+tempTitl = "temptitle"
+tempForTest = {"title": tempTitl, "cyrentTime" : str(curentDay), "editTime": str(curentDay), "content": "1234124"}
+
+print(tempForTest)
+
+
+#with open('data.json', 'a') as f:
+ #   json.dump(tempForTest, f, indent='\t')
+
+#with open('data.json') as f:
+ #   templates = json.load(f)
+
+f = open('data.json')
+json = json.load(f)
+
+for i in json:
+    print(i + " : " +json[i])
+
+#date_time_obj = datetime.strptime(templates["cyrentTime"], '%Y-%m-%d %H:%M:%S.%f')
+
